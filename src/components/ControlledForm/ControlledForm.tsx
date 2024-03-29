25ca75cd789a4b2ef8c2e041484a1a6c4bade7d5
Luis Gabriel <luis16757@gmail.com>
2024-03-29 16:25:24 -0300

    change font size

--- src/components/ControlledForm/ControlledForm.tsx
+++ src/components/ControlledForm/ControlledForm.tsx
@@ -39,6 +39,7 @@ const ControlledFormContainer = styled.section`
   div {
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
     background: #eeb06e;
+    font-size: 20px;
     border-radius: 20px;
     padding: 20px;
     height: 200px;
