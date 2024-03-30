2ba6368529773bf9aa414a9a35a82247d934450c
Luis Gabriel <luis16757@gmail.com>
2024-03-30 14:25:30 -0300

    fix: resolve child component

--- src/components/Child/Child.tsx
+++ src/components/Child/Child.tsx
@@ -1,10 +1,34 @@
+import styled from "styled-components";
 import Button from "../Button/Button";
 
-const Child = ({incrementNumber}: {incrementNumber: () => void}) => {
+interface IChildProps {
+  parentNumber: number
+  incrementNumber: ()=> void
+}
+
+const ShieldContainer = styled.div`
+background: #f4f4f4;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  max-width: 500px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  p {
+    margin: 10px;
+    font-size: 1.8rem;
+  }
+  `
+
+const Child = ({incrementNumber, parentNumber}: IChildProps) => {
     return (
-        <div>
-          <Button onClick={incrementNumber}>Incrementar Número no Parent</Button>
-        </div>
+        <ShieldContainer>
+          <p>{parentNumber}</p>
+          <Button onClick={incrementNumber}>Incrementar Número</Button>
+        </ShieldContainer>
       );
 }
 
--- src/components/Parent/Parent.tsx
+++ src/components/Parent/Parent.tsx
@@ -1,24 +1,5 @@
 import { useState } from 'react'
 import Child from '../Child/Child';
-import styled from 'styled-components';
-
-const ParentContainer = styled.div`
-  background: #f4f4f4;
-  border-radius: 8px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  padding: 20px;
-  max-width: 500px;
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  p {
-    margin: 10px;
-    font-size: 2rem;
-  }
-`;
 
 const Parent = () => {
     const [number, setNumber] = useState<number>(0);
@@ -28,10 +9,9 @@ const Parent = () => {
     };
   
     return (
-      <ParentContainer>
-        <p>{number}</p>
-        <Child incrementNumber={incrementNumber} />
-      </ParentContainer>
+      <div>
+        <Child incrementNumber={incrementNumber} parentNumber={number} />
+      </div>
     );
   };
 
